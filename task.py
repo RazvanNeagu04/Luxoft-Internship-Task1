33a8af4c084bcd47e4fc01348fa464613fa78197
Razvan Neagu <razvanneagu2@gmail.com>
2022-07-11 11:22:55 +0300

    Creating conflict

--- task.py
+++ task.py
@@ -1,12 +1,4 @@
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver-manager import ChromeDriverManager
-#
-# session = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-# session.get(url = "https://www.luxoft.com")
-#
-# time.sleep(5)
+
 import time
 
 from selenium import webdriver
@@ -15,5 +7,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 session = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 session.get(url="https://www.luxoft.com")
+
+session.maximize_window()
+time.sleep(10)
+
+button = session.find_element_by_link_text("Careers")
+button.click
+
+session.find_element()
 
 time.sleep(5)
