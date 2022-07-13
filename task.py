f5d2ddd1d67ce4949f7ab9e9c44921a0597c882d
MihaiPopovici <mpopovici2000@gmail.com>
2022-07-13 10:09:47 +0300

    First commit in mihai branch

--- task.py
+++ task.py
@@ -1,20 +1,18 @@
-
 import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-session = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+session =webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 session.get(url="https://www.luxoft.com")
-
 session.maximize_window()
-time.sleep(10)
+time.sleep (5)
 
-button = session.find_element_by_link_text("Careers")
-button.click
-
-session.find_element()
-
+button = session.find_element(by=By.LINK_TEXT,value="CAREERS")
+button.click()
 time.sleep(5)
 
+button = session.find_element(by=By.LINK_TEXT,value="SEARCH JOBS")
+button.click()
+time.sleep(2)
