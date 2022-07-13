4fecee0678a3a2c7795e34bdda19d82c03e5301a
Razvan Neagu <razvanneagu2@gmail.com>
2022-07-13 09:54:52 +0300

    First commit in branch razvan

--- task.py
+++ task.py
@@ -1,20 +1,49 @@
-
+import select
 import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
 from webdriver_manager.chrome import ChromeDriverManager
 
 session = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 session.get(url="https://www.luxoft.com")
 
 session.maximize_window()
-time.sleep(10)
+time.sleep(3)
 
-button = session.find_element_by_link_text("Careers")
-button.click
+#button = WebDriverWait(session, 10).until(ec.element_to_be_clickable((By.XPATH, "//a[text()='CAREERS']")))
 
-session.find_element()
+button = session.find_element(by=By.LINK_TEXT,value="CAREERS")
+button.click()
+
+time.sleep(3)
+
+button = session.find_element(by = By.CLASS_NAME, value = "career-banner-button")
+button.click()
+
+time.sleep(5)
+
+button = session.find_element(by = By.XPATH, value = "//select[@id= 'search_countries']/..")
+button.click()
+#button = Select(session.find_element(by = By.NAME, value = "countryID[]" ))
+#button = Select(session.find_element(by = By.XPATH, value = "//span[contains(@aria-owns, 'search_countries')]//input"))
+#button.select_by_visible_text()
+countries = session.find_element(by = By.XPATH, value = "//span[contains(@aria-owns, 'search_countries')]//input")
+countries.send_keys("RO")
+countries.send_keys(Keys.ENTER)
+time.sleep(5)
+
+
+
+
+
+
+
 
 time.sleep(5)
 
