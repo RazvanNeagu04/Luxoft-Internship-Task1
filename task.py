97acbef6762adff41d9052dca33b362e264863a2
MihaiPopovici <mpopovici2000@gmail.com>
2022-07-15 11:36:20 +0300

    A

--- task.py
+++ task.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 session =webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 session.get(url="https://www.luxoft.com")
@@ -15,4 +16,23 @@ time.sleep(5)
 
 button = session.find_element(by=By.LINK_TEXT,value="SEARCH JOBS")
 button.click()
+time.sleep(5)
+
+#button = session.find_element(by = By.XPATH, value = "//select[@id= 'search_countries']/..")
+button = session.find_element(by = By.XPATH, value = "//span[contains(text(),'Countries')]/..")
+coordinates = button.location_once_scrolled_into_view
 time.sleep(2)
+session.execute_script(f"window.scrollTo({coordinates['x']}, {coordinates['y'] -300})")
+
+button.click()
+
+countries = session.find_element(by = By.XPATH, value = "//span[contains(@aria-owns, 'search_countries')]//input")
+countries.send_keys("RO")
+
+countries.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+# "//button[contains(@class,'career-jobs')]"
+# "//button[@id = 'save-country-ids']"
+# //button[text()='Search']
